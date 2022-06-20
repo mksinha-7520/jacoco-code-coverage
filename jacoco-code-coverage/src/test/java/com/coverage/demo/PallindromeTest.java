e5b5d3e34a73a453dc289b6cfc81275f1a688e5c
Sinha <manish.kumar.sinha@ing.com>
2022-06-20 09:43:24 +0200

    pallindrom method modified

--- jacoco-code-coverage/src/test/java/com/coverage/demo/PallindromeTest.java
+++ jacoco-code-coverage/src/test/java/com/coverage/demo/PallindromeTest.java
@@ -3,11 +3,24 @@ package com.coverage.demo;
 import com.coverage.demo.Pallindrome;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class PallindromeTest {
     @Test
     public void whenEmptyString_thenAccept() {
         Pallindrome palindromeTester = new Pallindrome();
         assertTrue(palindromeTester.isPalindrome(""));
+    }
+
+    @Test
+    public void whenNonEmptyPallindromeString_thenAccept() {
+        Pallindrome palindromeTester = new Pallindrome();
+        assertTrue(palindromeTester.isPalindrome("ana"));
+    }
+
+    @Test
+    public void whenNonEmptyNonPallindromeString_thenAccept() {
+        Pallindrome palindromeTester = new Pallindrome();
+        assertFalse(palindromeTester.isPalindrome("and"));
     }
 }
